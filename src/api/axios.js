b4870bafaeb78946fe6e816fd87045da3604eabf
BendriChaithanya <chaithanyabendri@gmail.com>
2026-07-18 19:40:34 +0530

    Update frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://bankbackend-n4ey.onrender.com"
+    baseURL: import.meta.env.VITE_API_URL || "bankbackend-production.up.railway.app"
 });
 
 API.interceptors.request.use((config) => {
